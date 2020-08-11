78ed91daa245ca6a8ed72a331915ad5fafd2d0b0
DariusPirvulescu <organicdarius@gmail.com>
2020-08-11 20:27:32 +0300

    formatted code in App

---
+++ src/components/App/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.scss";
+import Top from "../Top";
+
+function App() {
+  return (
+    <div className="App">
+      <Top />
+      
+    </div>
+  );
+}
+
+export default App;
