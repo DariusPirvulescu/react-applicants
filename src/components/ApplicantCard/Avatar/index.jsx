b4ae537de3e5484f86a668a4e04a6aaed1b4540d
DariusPirvulescu <organicdarius@gmail.com>
2020-08-13 01:50:07 +0300

    implemented random avatar color

--- src/components/ApplicantCard/Avatar/index.jsx
+++ src/components/ApplicantCard/Avatar/index.jsx
@@ -1,24 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Avatar.scss";
-import setColor from "./ColorPicker";
 
-const avatarColor = setColor();
-const avatarStyle = {
-  backgroundColor: avatarColor[0],
-};
+class Avatar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      customColors: [
+        ["#E3E7F9", "#405493"],
+        ["#CEF0F4", "#34ADC6"],
+        ["#F0F4E1", "#ACAF36"],
+        ["#FEE8D3", "#E97428"],
+      ],
+      avatarColors: [],
+    };
+  }
 
-const initialsStyle = {
-  color: avatarColor[1],
-};
+  componentDidMount() {
+    this.colorPicker();
+  }
 
-const Avatar = ({ initials }) => {
-  return (
-    <div className="avatar-circle" style={avatarStyle}>
-      <span className="initials" style={initialsStyle}>
-        {initials}
-      </span>
-    </div>
-  );
-};
+  colorPicker() {
+    const { customColors } = this.state;
+    const randomColor =
+      customColors[Math.floor(Math.random() * customColors.length)];
+    this.setState({
+      avatarColors: randomColor,
+    });
+  }
+
+  render() {
+    const { avatarColors } = this.state;
+    const { initials } = this.props;
+    const avatarStyle = {
+      backgroundColor: avatarColors[0],
+    };
+
+    const initialsStyle = {
+      color: avatarColors[1],
+    };
+    return (
+      <div className="avatar-circle" style={avatarStyle}>
+        <span className="initials" style={initialsStyle}>
+          {initials}
+        </span>
+      </div>
+    );
+  }
+}
 
 export default Avatar;
