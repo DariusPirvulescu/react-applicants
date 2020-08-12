6bbf3db98034520dc06d958fe42acd9a3c75cb05
DariusPirvulescu <organicdarius@gmail.com>
2020-08-12 10:44:40 +0300

    changed Input component to StyledInput using styled-components

--- src/components/Search/Input/index.jsx
+++ src/components/Search/Input/index.jsx
@@ -3,32 +3,31 @@ import "./Input.scss";
 import SearchIcon from "@material-ui/icons/Search";
 import TextField from "@material-ui/core/TextField";
 import InputAdornment from "@material-ui/core/InputAdornment";
-import { makeStyles } from "@material-ui/core/styles";
+import styled from "styled-components";
 
-const useStyles = makeStyles({
-  root: {
-    border: "1px solid #E6E6E6;",
-    width: "100%",
-  },
-});
+const StyledTextField = styled(TextField)`
+  & ::-webkit-input-placeholder {
+    font-size: 1.4rem;
+  }
+`;
 
 const SearchInput = () => {
-  const classes = useStyles();
-
   return (
-    <TextField
-      className={classes.root}
-      InputProps={{
-        startAdornment: (
-          <InputAdornment position="start">
-            <SearchIcon />
-          </InputAdornment>
-        ),
-      }}
-      placeholder="Search for applicant"
-      variant="outlined"
-      size="medium"
-    />
+    <div>
+      <StyledTextField
+        className="search-input"
+        variant="outlined"
+        placeholder="Search for applicant"
+        InputProps={{
+          style: { fontSize: "1.4rem" },
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+      />
+    </div>
   );
 };
 
