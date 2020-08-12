0f837c12b8d70ac0a4f2ed35c2e3297a5f47c892
DariusPirvulescu <organicdarius@gmail.com>
2020-08-12 14:22:50 +0300

    imported applicants.json and added the ApplicantCard with all the props

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,12 +2,26 @@ import React from "react";
 import "./App.scss";
 import Top from "../Top";
 import Search from "../Search";
+import applicants from "../../data/applicants.json";
+import ApplicantCard from "../ApplicantCard";
 
 function App() {
   return (
     <div className="App">
       <Top />
       <Search />
+
+      <ApplicantCard
+        firstName="Joseph"
+        lastName="Francisk"
+        phone="+49 212 554 3232"
+        email="fr.joseph@gmail.com"
+        status="APPOINTMENT"
+        date="22 JULY"
+        time="14:00"
+        bid="false"
+        bidAmount="0"
+      />
     </div>
   );
 }
