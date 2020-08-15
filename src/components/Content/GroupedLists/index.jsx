b06366dc82dcaa4c5c11335a4aeb295f95bdc827
DariusPirvulescu <organicdarius@gmail.com>
2020-08-15 23:29:23 +0300

    fixed colors change after each search

--- src/components/Content/GroupedLists/index.jsx
+++ src/components/Content/GroupedLists/index.jsx
@@ -5,11 +5,9 @@ const GroupedList = ({ data }) => {
   return (
     <div>
       {Object.entries(data).map((list) => {
-        let key = (list[0] + Math.floor(Math.random() * 99));
-        console.log(key)
         return (
           list[1].length !== 0 && (
-            <List key={key} title={list[0]} applicants={list[1]} />
+            <List key={list[0]} title={list[0]} applicants={list[1]} />
           )
         );
       })}
