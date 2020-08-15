e23eaa675287cb2812634f1818da28a9b2b554b0
DariusPirvulescu <organicdarius@gmail.com>
2020-08-16 02:34:42 +0300

    added redirect to '/page' route

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.scss";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Content from "../Content";
 
 const App = () => {
@@ -8,6 +8,9 @@ const App = () => {
     <div className="App">
       <Switch>
         <Route exact path="/page/" component={Content} />
+        <Route path="/">
+          <Redirect to="/page" />
+        </Route>
         {/* <Route component={Error} /> */}
       </Switch>
     </div>
