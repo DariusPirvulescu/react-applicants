ac4ed11945fd1c5ed6dca2bb29a47718d18b68f0
DariusPirvulescu <organicdarius@gmail.com>
2020-08-16 23:43:26 +0300

    added Menu in App. Minor scss styling

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.scss";
 import { Switch, Route, Redirect } from "react-router-dom";
+import Menu from "../Menu";
 import Content from "../Content";
 import Footer from "../Footer";
 
 const App = () => {
   return (
     <div className="App">
+      <Menu />
       <Switch>
         <Route exact path="/page/" component={Content} />
         <Route path="/">
