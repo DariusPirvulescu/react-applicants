2e57610ff8204002d6c31c0ba70119fd885eaff9
DariusPirvulescu <organicdarius@gmail.com>
2020-08-17 21:38:51 +0300

    fixed space in Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,8 +5,8 @@ const Footer = () => {
   return (
     <div className="footer">
       <p>
-        <a href="https://www.mcmakler.de/agb">AGB</a> &#8226;
-        <a href="https://www.mcmakler.de/datenschutz"> Datenschutz </a> &#8226;
+        <a href="https://www.mcmakler.de/agb">AGB</a> &#8226;{" "}
+        <a href="https://www.mcmakler.de/datenschutz">Datenschutz</a> &#8226;{" "}
         <a href="https://www.mcmakler.de/impressum">Impressum</a>
       </p>
     </div>
